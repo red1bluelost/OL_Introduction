1df7db1b78b9ea4dc8fb4b36ee6cfd0bb0d43677
red1bluelost <msweston12@cox.net>
2020-01-17 16:27:46 -0500

    Adds enumeration for flags, parsing points, images, and videos, and range based templating.

--- page/page.go
+++ page/page.go
@@ -1,41 +1,105 @@
 package page
 
 import (
-	"github.com/red1bluelost/OL_Introduction/image"
+	"bytes"
+	"fmt"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"reflect"
 )
 
-type Pager interface {
-	BodyParse(df []byte) error
+type Flag int
+
+const (
+	POINT Flag = 0
+	IMAGE Flag = 1
+	VIDEO Flag = 2
+)
+
+func (f Flag) FlagBytes() []byte {
+	flags := [...][]byte{
+		[]byte("(p)"),
+		[]byte("(i)"),
+		[]byte("(v)"),
+	}
+	return flags[f]
+}
+
+type Page struct {
+	Title string
+	Point [][]byte
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
+	title := r.URL.Path[len("/"):]
 	p, err := handleLoadPage(title)
 	if err != nil {
 		log.Panicf("Handler failed %s", err)
 	}
-	switch p := p.(type) {
-	case *image.ImagePage:
-		t, _ := template.ParseFiles("kirby.html")
-		t.Execute(w, p)
-	default:
-		log.Panicf("cannot handle page of type (%s)", reflect.TypeOf(p))
-	}
+	t, _ := template.ParseFiles("kirby.html")
+	t.Execute(w, p)
 }
 
-func handleLoadPage(title string) (Pager, error) {
-	switch title[:5] {
-	case "image":
-		title = title[6:]
-		page, err := image.LoadImagePage(title)
-		return page, err
-	default:
-		panic("handleLoadPage failed")
-	}
-	return nil, nil
+func handleLoadPage(title string) (*Page, error) {
+	title = title[:]
+	page, err := loadPage(title)
+	return page, err
 }
 
+func loadPage(title string) (*Page, error) {
+	filename := "data/" + title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	page := new(Page)
+	ok := page.BodyParse(body)
+	if ok != nil {
+		panic(ok)
+	}
+	page.Title = title
+	return page, nil
+}
+
+func (p *Page) BodyParse(df []byte) error {
+	for len(df) > 3 {
+		end := 0
+		var curFlag Flag
+		for j := 0; j < len(df) ; j++ {
+			var ok bool
+			curFlag, ok = checkFlag(df[j:(j + 3)])
+			if ok {
+				end = j + 3
+				break
+			}
+		}
+		switch curFlag {
+		case POINT:
+			p.Point = append(p.Point, df[:(end-3)])
+		case IMAGE:
+			panic("implement this")
+		case VIDEO:
+			panic("implement this")
+		}
+		df = df[end:]
+		fmt.Printf("%s\n", df)
+	}
+	return nil
+}
+
+func checkFlag(df []byte) (Flag, bool) {
+	ok := bytes.Compare(POINT.FlagBytes(), df)
+	if ok == 0 {
+		return POINT, true
+	}
+	ok = bytes.Compare(IMAGE.FlagBytes(), df)
+	if ok == 0 {
+		return IMAGE, true
+	}
+	ok = bytes.Compare(VIDEO.FlagBytes(), df)
+	if ok == 0 {
+		return VIDEO, true
+	}
+	return 0, false
+}
--- presentation.go
+++ presentation.go
@@ -6,23 +6,18 @@ import (
 	"net/http"
 )
 
-
 func main() {
-	http.HandleFunc("/view/", page.Handler)
+	http.HandleFunc("/", page.Handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 //func main() {
-//	testpage := &Page{
-//		Title:  "Fuck",
-//		Point1: nil,
-//		Point2: nil,
-//		Point3: nil,
+//	testpage := &page.Page{Title: "Fuck"}
+//	testpage.Point = make([][]byte, 0)
+//	testparse := []byte("shit(p)fuck(p)ass(p)")
+//	testpage.BodyParse(testparse)
+//
+//	for i := 0; i < 3; i++ {
+//		fmt.Printf("%d: %s\n", i, testpage.Point[i])
 //	}
-//	testparse := []byte("(p)shit(p)fuck(p)ass(p)")
-//	testpage.bodyParse(testparse)
-//	fmt.Printf("1: %s, 2: %s, 3: %s",
-//		testpage.Point1, testpage.Point2, testpage.Point3)
 //}
-
-
